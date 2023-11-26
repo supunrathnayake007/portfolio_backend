394c067516f048ff6994f54a26f44fd25cfc52a4
Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-11-26 12:44:36 +0530

    next.config.js file changed for images not visible issue

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   output: "export",
   // Add basePath
   basePath: "/github-pages",
+  images: {
+    loader: "custom",
+    loaderFile: "./my-loader.ts",
+  },
 };
 
 module.exports = nextConfig;
