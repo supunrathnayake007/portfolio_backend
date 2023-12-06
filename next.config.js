8696e6c3353a4c7f3dc495cb3015b6fb7f79cb9a
Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-12-06 16:55:01 +0530

    github pages login page error trying fix 3

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //output: "export",
+  output: "export",
   //Add basePath
-  //basePath: "/github-pages",
+  basePath: "/github-pages",
 };
 
 module.exports = nextConfig;
