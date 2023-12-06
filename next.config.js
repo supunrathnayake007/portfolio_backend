73ff12638fdb56e1327c44e04587f1d56d8404ef
Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-12-06 17:23:14 +0530

    github pages login page error trying fix 10

--- next.config.js
+++ next.config.js
@@ -1,29 +1,17 @@
-// /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   //Add basePath
   basePath: "/github-pages",
-};
-
-// module.exports = nextConfig;
-
-// // module.exports = {
-// //   productionBrowserSourceMaps: true,
-// // };
-
-const debug = process.env.NODE_ENV !== "production";
-
-module.exports = {
-  exportPathMap: function () {
-    // /Next-React-Components
-    return {
-      "/": { page: "/" },
-      "/ap-grid-layout": { page: "/ap-grid-layout" },
-      "/ap-highlight": { page: "/ap-highlight" },
-    };
-  },
   assetPrefix: !debug
     ? "https://supunrathnayake007.github.io/social_media_clone/"
     : "",
 };
+
+module.exports = nextConfig;
+
+// // module.exports = {
+// //   productionBrowserSourceMaps: true,
+// // };
