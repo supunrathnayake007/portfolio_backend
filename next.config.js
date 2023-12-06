06346a36f450e7e0fcfd058d183c6477a92a5016
Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-12-06 17:16:59 +0530

    github pages login page error trying fix 7

--- next.config.js
+++ next.config.js
@@ -1,14 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export",
-  //Add basePath
-  //basePath: "/github-pages",
-  basePath: "/social_media_clone",
-};
-
-module.exports = nextConfig;
-
-// module.exports = {
-//   productionBrowserSourceMaps: true,
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: "export",
+//   //Add basePath
+//   //basePath: "/github-pages",
+//   basePath: "/social_media_clone",
 // };
+
+// module.exports = nextConfig;
+
+// // module.exports = {
+// //   productionBrowserSourceMaps: true,
+// // };
+
+const debug = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  exportPathMap: function () {
+    // /Next-React-Components
+    return {
+      "/": { page: "/" },
+      "/ap-grid-layout": { page: "/ap-grid-layout" },
+      "/ap-highlight": { page: "/ap-highlight" },
+    };
+  },
+  assetPrefix: !debug
+    ? "https://anotherplanet-io.github.io/Next-React-Components/"
+    : "",
+};
