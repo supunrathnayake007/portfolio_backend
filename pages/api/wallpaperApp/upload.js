be3e38329a6357214fc7b1775d27a4b544f2602c
Supun Rathnayake <supun.rathnayake007@gmail.com>
2024-02-16 10:01:52 +0530

    wallpaper app API  CORS issue

--- pages/api/wallpaperApp/upload.js
+++ pages/api/wallpaperApp/upload.js
@@ -11,6 +11,9 @@ export const config = {
 };
 
 export default function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   try {
     upload.any("wallpaper")(req, res, async (err) => {
       if (err) {
